24fb14826c047fab99d9e08f601ffdbd4dfc3620
Nick VanMeter <nickjvm@gmail.com>
2025-07-09 08:51:56 -0600

    add timelapse project card + misc cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,19 +12,34 @@ export default function Home() {
           <nav>
             <ul className="flex gap-8 font-xl">
               <li>
-                <a href="https://github.com/nickjvm" target="_blank" rel="noopener noreferrer" className="hover:text-white/75 transition focus:text-white/75">
+                <a
+                  href="https://github.com/nickjvm"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:text-white/75 transition focus:text-white/75"
+                >
                   <FaGithub className="social-icon" />
                   <span className="sr-only">github</span>
                 </a>
               </li>
               <li>
-                <a href="https://www.linkedin.com/in/nickvanmeter" target="_blank" rel="noopener noreferrer" className="hover:text-white/75 transition focus:text-white/75">
+                <a
+                  href="https://www.linkedin.com/in/nickvanmeter"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:text-white/75 transition focus:text-white/75"
+                >
                   <FaLinkedin className="social-icon" />
                   <span className="sr-only">linkedin</span>
                 </a>
               </li>
               <li>
-                <a href="mailto:nick@vanmeter.dev" target="_blank" rel="noopener noreferrer" className="hover:text-white/75 transition focus:text-white/75">
+                <a
+                  href="mailto:nick@vanmeter.dev"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:text-white/75 transition focus:text-white/75"
+                >
                   <MdOutlineAlternateEmail className="social-icon" />
                   <span className="sr-only">email</span>
                 </a>
@@ -40,14 +55,19 @@ export default function Home() {
             <div className="h-full w-full gridded flex items-center ">
               <div className="relative w-full md:max-w-260 ml-auto md:px-24 px-6 text-center md:text-left pt-24 pb-8 md:pt-0 md:pb-0">
                 <h2 className="text-slate-800 inline-block">
-                  <span className="
+                  <span
+                    className="
                 lg:text-[100px] md:text-[10vw] sm:text-[60px] text-[48px] 
                 lg:leading-[75px] md:leading-[7.5vw] sm:leading-[45px] leading-[36px]
-                font-display  tracking-tighter inline-block">
-                    nick<br className="hidden md:block" /> vanmeter
+                font-display  tracking-tighter inline-block"
+                  >
+                    nick
+                    <br className="hidden md:block" /> vanmeter
                   </span>
-                  <div className="w-full flex justify-between text-left text-slate-500
-                lg:text-lg md:text-md text-sm">
+                  <div
+                    className="w-full flex justify-between text-left text-slate-500
+                lg:text-lg md:text-md text-sm"
+                  >
                     <span>front-end engineer</span>
                     <span>react developer</span>
                     <span>tech enthusiast</span>
@@ -58,9 +78,14 @@ export default function Home() {
           </div>
           <div className="col-span-4 md:col-span-1 lg:col-span-1 bg-[#7A4D9F] max-h-[200px] md:max-h-full md:h-full flex items-center order-1 md:order-2 py-18 md:py-0">
             <div className="gridded h-full w-full flex items-center">
-
               <div className="w-full md:max-w-100 text-center">
-                <Image src="/avatar.png" alt="nick vanmeter profile photo" width={400} height={400} className="relative md:-left-1/2 inline translate-y-[38%] md:translate-y-0 md:mb-0 md:w-auto max-w-full w-[200px] rounded-full md:rounded" />
+                <Image
+                  src="/avatar.png"
+                  alt="nick vanmeter profile photo"
+                  width={400}
+                  height={400}
+                  className="relative md:-left-1/2 inline translate-y-[38%] md:translate-y-0 md:mb-0 md:w-auto max-w-full w-[200px] rounded-full md:rounded"
+                />
               </div>
             </div>
           </div>
@@ -71,39 +96,111 @@ export default function Home() {
           <div className="col-span-4 space-y-12">
             <div className="space-y-4 max-w-3xl mx-auto">
               <div className="text-center space-y-2 flex flex-col items-center justify-center mb-12">
-                <p className="rounded font-bold md:text-5xl sm:text-3xl text-xl font-display bg-slate-800 text-white px-2 inline-block">senior front-end engineer</p>
-                <p className="rounded font-bold md:text-2xl sm:text-xl text-md font-display bg-slate-800 text-white px-2 inline-block">user experience, design & development</p>
-                <p className="rounded font-bold md:text-sm sm:text-xs text-xs font-display bg-slate-800 text-white px-2 inline-block">(and dogs, sourdough, trails & crochet)</p>
+                <p className="rounded font-bold md:text-5xl sm:text-3xl text-xl font-display bg-slate-800 text-white px-2 inline-block">
+                  senior front-end engineer
+                </p>
+                <p className="rounded font-bold md:text-2xl sm:text-xl text-md font-display bg-slate-800 text-white px-2 inline-block">
+                  user experience, design & development
+                </p>
+                <p className="rounded font-bold md:text-sm sm:text-xs text-xs font-display bg-slate-800 text-white px-2 inline-block">
+                  (and dogs, sourdough, trails & crochet)
+                </p>
               </div>
               <div className="px-4 lg:px-0 space-y-4">
-                <p>From SaaS products and e-commerce sites to developer tools, I&apos;m all about crafting smooth, intuitive user experiences. Working with a team of product managers/owners, designers and other engineers is where I do my best work.</p>
-                <p>I&apos;m driven by the constantly evolving frontend landscape (especially seeing AI pop up in user experiences in cool and innovative ways). I enjoy tinkering with fun little projects like what you see below — they&apos;re my playgrounds for experimenting with interesting tech and creative product ideas.</p>
+                <p>
+                  From SaaS products and e-commerce sites to developer tools,
+                  I&apos;m all about crafting smooth, intuitive user
+                  experiences. Working with a team of product managers/owners,
+                  designers and other engineers is where I do my best work.
+                </p>
+                <p>
+                  I&apos;m driven by the constantly evolving frontend landscape
+                  (especially seeing AI pop up in user experiences in cool and
+                  innovative ways). I enjoy tinkering with fun little projects
+                  like what you see below — they&apos;re my playgrounds for
+                  experimenting with interesting tech and creative product
+                  ideas.
+                </p>
               </div>
-
             </div>
             <div className="relative w-full grid md:grid-cols-3 gap-10 px-4 lg:px-0">
               <ProjectCard
-                title="reciparse"
+                title="🍽️ reciparse"
                 href="https://reciparse.com"
                 source="https://github.com/nickjvm/reciparse"
                 description="Recipe viewer that extracts and displays clean, clutter-free recipe content from popular food websites. It pulls in the essential details and removes distractions like intrusive ads and annoying popups."
-                tags={["react", "next.js", "typescript", "tailwind", "node", "express", "vercel"]} />
+                tags={[
+                  "react",
+                  "next.js",
+                  "typescript",
+                  "tailwind",
+                  "node",
+                  "express",
+                  "vercel",
+                ]}
+              />
               <ProjectCard
-                title="sync shop"
+                title="🛒 sync shop"
                 href="https://socket-shopping.vanmeter.dev/"
                 source="https://github.com/nickjvm/socket-shopping-list"
                 description="Minimalist, collaborative shopping list app inspired by the simplicity of iOS Reminders. It uses generative AI to automatically categorize items as they’re added and enables real-time updates across shared lists."
-                tags={["react", "next.js", "typescript", "tailwind", "socket.io", "gen ai", "railway", "docker", "draggable"]} />
+                tags={[
+                  "react",
+                  "next.js",
+                  "typescript",
+                  "tailwind",
+                  "socket.io",
+                  "gen ai",
+                  "railway",
+                  "docker",
+                  "draggable",
+                ]}
+              />
               <ProjectCard
-                title="taste match"
+                title="📸 timelapse"
+                href="https://timelapse.vanmeter.dev/"
+                source="https://github.com/nickjvm/timelapse"
+                description="An app that simplifies the creation of timelapses from a series of images. All data is stored locally via IndexedDB. Images are compressed and base64 encoded, and comparison views and edited images can be exported as pngs."
+                tags={[
+                  "react",
+                  "next.js",
+                  "typescript",
+                  "tailwind",
+                  "indexeddb",
+                  "railway",
+                  "docker",
+                  "draggable",
+                  "canvas",
+                  "framer/motion",
+                ]}
+              />
+              <ProjectCard
+                title="🍔 taste match"
                 href="https://restaurant-matcher.vanmeter.dev/"
                 source="https://github.com/nickjvm/restaurant-matcher"
                 description="Tinder-style restaurant pairing game designed to answer the age-old question: “Where should we eat tonight?”"
-                tags={["react", "next.js", "typescript", "tailwind", "framer/motion", "node", "socket.io", "railway", "docker"]} />
+                tags={[
+                  "react",
+                  "next.js",
+                  "typescript",
+                  "tailwind",
+                  "framer/motion",
+                  "node",
+                  "socket.io",
+                  "railway",
+                  "docker",
+                ]}
+              />
+              <ProjectCard
+                title="🖨️ trainerize extension"
+                source="https://github.com/nickjvm/trainerize-extension"
+                description="An interesting way to experiment with the browser extension API, this extension generates a printer-friendly report of the last 7 days of meal logs."
+                tags={["typescript", "browser extension API"]}
+              />
             </div>
           </div>
         </div>
       </div>
-    </main >
+    </main>
   );
 }
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,70 +1,95 @@
-'use client'
+"use client";
 
 import { useEffect, useRef, useState } from "react";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { FaExternalLinkSquareAlt, FaGithub } from "react-icons/fa";
 
 type Props = {
-    title: string,
-    href: string,
-    source: string,
-    description: string,
-    tags: string[]
-}
-export default function ProjectCard({ title, href, source, description, tags }: Props) {
-    const [online, setOnline] = useState<boolean | null>(null)
-    const intervalRef = useRef<NodeJS.Timeout | null>(null)
-    const retryCount = useRef<number>(0);
-    useEffect(() => {
-        const checkStatus = () => {
-            fetch('/api/appStatus?url=' + href).then(r => {
-                if (r.ok) {
-                    setOnline(true)
-                    if (intervalRef.current) {
-                        clearInterval(intervalRef.current)
-                    }
-                } else {
-                    retryCount.current += 1
-                    if (retryCount.current < 5) {
-                        intervalRef.current = setInterval(() => {
-                            checkStatus()
-                        }, 5000)
-                    }
-                }
-            })
+  title: string;
+  href?: string;
+  source: string;
+  description: string;
+  tags: string[];
+};
+export default function ProjectCard({
+  title,
+  href,
+  source,
+  description,
+  tags,
+}: Props) {
+  const [online, setOnline] = useState<boolean | null>(null);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+  const retryCount = useRef<number>(0);
+  useEffect(() => {
+    if (!href) {
+      return;
+    }
+    const checkStatus = () => {
+      fetch("/api/appStatus?url=" + href).then((r) => {
+        if (r.ok) {
+          setOnline(true);
+          if (intervalRef.current) {
+            clearInterval(intervalRef.current);
+          }
+        } else {
+          retryCount.current += 1;
+          if (retryCount.current < 5) {
+            intervalRef.current = setInterval(() => {
+              checkStatus();
+            }, 5000);
+          }
         }
-        checkStatus()
-    }, [href])
+      });
+    };
+    checkStatus();
+  }, [href]);
 
-    return (
-        <div className="text-slate-900 space-y-4 h-full">
-            <div className="space-y-2">
-                <h2 className="text-2xl font-display">{title}</h2>
-                <p>{description || 'lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'}</p>
-            </div>
-            <div className="flex flex-wrap gap-x-2 text-sm text-slate-500">
-                {tags.map((tag, index) => (
-                    <span className="whitespace-nowrap" key={index}>{tag}</span>
-                ))}
-            </div>
-            <div className="space-y-2 items-center mt-auto">
-                {online !== false && (
-                    <a href={href}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="h-10.5 relative rounded flex-1/2 flex items-center justify-center gap-2 p-2 bg-slate-800 text-white group after:content-[''] after:bg-white/10 after:absolute after:bottom-0 after:left-0 after:top-0 after:block after:w-[0px] hover:after:w-full after:mix-blend-screen after:transition-[width] after:duration-300">
-                        {online !== true ? <AiOutlineLoading3Quarters className="w-6 h-6 animate-spin" /> : (
-                            <>
-                                <FaExternalLinkSquareAlt className="w-4 h-4" />
-                                Check it out
-                            </>
-                        )}
-                    </a>
-                )}
-                <a href={source} target="_blank" rel="noopener noreferrer" className="h-10.5 relative rounded flex items-center justify-center gap-2 flex-1/2 p-2 border border-slate-800 text-slate-800 group after:content-[''] after:bg-slate-800 after:absolute after:bottom-0 after:left-0 after:top-0 after:block after:w-[0px] after:mix-blend-soft-light hover:after:w-full after:transition-[width] after:duration-300">
-                    <FaGithub className="w-4 h-4" />
-                    Github repo</a>
-            </div>
-        </div>
-    )
+  return (
+    <div className="text-slate-900 space-y-4 h-full">
+      <div className="space-y-2">
+        <h2 className="text-xl font-display">{title}</h2>
+        <p>{description}</p>
+      </div>
+      <div className="flex flex-wrap gap-x-2 text-sm text-slate-500">
+        {tags.map((tag, index) => (
+          <span className="whitespace-nowrap" key={index}>
+            {tag}
+            {index < tags.length - 1 && ", "}
+          </span>
+        ))}
+      </div>
+      <div className="space-y-2 items-center mt-auto">
+        {href && online !== false && (
+          <a
+            href={online ? href : "#"}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="h-10.5 relative rounded flex-1/2 flex items-center justify-center gap-2 p-2 bg-slate-800 text-white group after:content-[''] after:bg-white/10 after:absolute after:bottom-0 after:left-0 after:top-0 after:block after:w-[0px] hover:after:w-full after:mix-blend-screen after:transition-[width] after:duration-300"
+          >
+            {online !== true ? (
+              <>
+                <AiOutlineLoading3Quarters className="w-6 h-6 animate-spin" />
+                <span className="sr-only">loading</span>
+              </>
+            ) : (
+              <>
+                <FaExternalLinkSquareAlt className="w-4 h-4" />
+                Check it out
+              </>
+            )}
+          </a>
+        )}
+        <a
+          href={source}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="h-10.5 relative rounded flex items-center justify-center gap-2 flex-1/2 p-2 border border-slate-800 text-slate-800 group after:content-[''] after:bg-slate-800 after:absolute after:bottom-0 after:left-0 after:top-0 after:block after:w-[0px] after:mix-blend-soft-light hover:after:w-full after:transition-[width] after:duration-300"
+        >
+          <FaGithub className="w-4 h-4" />
+          Github repo
+        </a>
+      </div>
+    </div>
+  );
 }
